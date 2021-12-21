71871ff182929c102c410b99ca7db45d21ee2abc
HenryDashwood <hcndashwood@gmail.com>
2021-12-21 18:43:20 +0000

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 PRIVATE_KEY=$1
 IP=$2
 
-scp [!.]* -r -i $PRIVATE_KEY ~/Documents/reader ubuntu@$IP:~/
+function killport() {
+  local port=$(lsof -t -i:"8000")
+  [[ -n $port ]] && kill $port
+}
+
+ssh -i $PRIVATE_KEY ubuntu@$IP "cd ~/reader && git pull"
 ssh -i $PRIVATE_KEY ubuntu@$IP "sudo cp -r ~/reader/src/frontend/* /var/www/reader.henrydashwood.com/"
+ssh -i $PRIVATE_KEY ubuntu@$IP "$(typeset -f killport); killport"
 ssh -i $PRIVATE_KEY ubuntu@$IP "cd ~/reader && ~/.pyenv/versions/py399/bin/python -m uvicorn src.backend.main:app &"
