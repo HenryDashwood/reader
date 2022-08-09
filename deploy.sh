96e34bf60609ceea8d3404acf77e72a32f079bd5
HenryDashwood <hcndashwood@gmail.com>
2022-08-09 23:16:26 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,31 @@ function killport() {
   [[ -n $port ]] && kill $port
 }
 
-ssh -i $PRIVATE_KEY ubuntu@$IP "cd ~/reader && git pull"
-ssh -i $PRIVATE_KEY ubuntu@$IP "sudo cp -r ~/reader/src/frontend/dist/* /var/www/reader.henrydashwood.com/"
-ssh -i $PRIVATE_KEY ubuntu@$IP "$(typeset -f killport); killport"
-ssh -i $PRIVATE_KEY ubuntu@$IP "cd ~/reader && ~/.pyenv/versions/3.10.5/envs/py3105/bin/python3.10 -m pip install -U pip"
-ssh -i $PRIVATE_KEY ubuntu@$IP "cd ~/reader && ~/.pyenv/versions/3.10.5/envs/py3105/bin/python3.10 -m pip install -U -r requirements.txt"
-ssh -i $PRIVATE_KEY ubuntu@$IP "cd ~/reader && ~/.pyenv/versions/py3105/bin/python -m uvicorn src.backend.main:app &"
+function updateCode() {
+  if [ -d "~/reader" ] 
+  then
+    cd ~/reader
+    git pull
+  else
+    git clone git@github.com:HenryDashwood/reader.git
+  fi
+}
 
+function buildBackend() {
+  cd ~/reader
+  ~/.pyenv/versions/3.10.5/envs/py3105/bin/python3.10 -m pip install -U pip
+  ~/.pyenv/versions/3.10.5/envs/py3105/bin/python3.10 -m pip install -U -r requirements.txt
+  ~/.pyenv/versions/py3105/bin/python -m uvicorn src.backend.main:app &
+}
+
+function buildFrontend() {
+  cd ~/reader/src/frontend
+  npx parcel build ./*.html
+  sudo cp -r ~/reader/src/frontend/dist/* /var/www/reader.henrydashwood.com/
+}
+
+
+ssh -i $PRIVATE_KEY ubuntu@$IP "$(typeset -f updateCode); updateCode"
+ssh -i $PRIVATE_KEY ubuntu@$IP "$(typeset -f killport); killport"
+ssh -i $PRIVATE_KEY ubuntu@$IP "$(typeset -f buildBackend); buildBackend"
+ssh -i $PRIVATE_KEY ubuntu@$IP "$(typeset -f buildFrontend); buildFrontend"
